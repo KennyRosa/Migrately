3601ee6d4536e9bf13deb9d43a9f9e1fb311e67d
Kenny Rosa <112502866+KennyRosa@users.noreply.github.com>
2023-01-27 21:24:03 -0500

    Create BottomStatement.jsx

---
+++ react/components/BottomStatement.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./landing.css";
+
+function BottomStatement() {
+  return (
+    <div className="container-landing-wave main-landing">
+      <p className="landing-last-statement">
+        The only digital platform you need for your U.S. travel, visa, or
+        immigration journey.
+      </p>
+    </div>
+  );
+}
+
+export default BottomStatement;
